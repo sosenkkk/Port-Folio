e64113742804f27d31ab1b37a1e4db8fc59ef60b
Shashank Raj <shashank3636@gmail.com>
2022-12-23 02:39:49 +0530

    Second

--- src/Components/Card/CardHolder.js
+++ src/Components/Card/CardHolder.js
@@ -6,7 +6,7 @@ const CardHolder = () => {
     return (
         <div style={{  display: "flex", justifyContent:"space-around", flexWrap:"wrap" }} >
             <ActionAreaCard toDisplay="Projects" toDescribe="All i have done is nothing" todo="projects" source="https://i.pinimg.com/originals/97/ae/28/97ae288bc10481a0152460e9a3c5faeb.gif" />
-            <ActionAreaCard toDisplay="Skills" toDescribe="All i have done is nothing" todo="hobby" />
+            <ActionAreaCard toDisplay="Skills" toDescribe="All i have done is nothing" todo="hobby" source="https://miro.medium.com/max/828/1*zlmwtg3fog11YXcU_rvfWA.gif" />
             <ActionAreaCard toDisplay="Experience" toDescribe="All i have done is nothing" todo="achieve" />
             <ActionAreaCard toDisplay="Achievements" toDescribe="All i have done is nothing" todo="achieve" />
         </div>
