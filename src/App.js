9b6dcbe67ea89421f030648e8c7daf18a381603d
Sosenk <109061152+sosenkkk@users.noreply.github.com>
2022-12-28 08:56:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ import CardHolder from "./Components/Card/CardHolder";
 import Preloader from "./Components/PreLoader/Preloader";
 import AboutMe from "./Components/About Me/AboutMe";
 import CartProvider from "./Components/Store/CartProvider";
-import CartContext from "./Components/Store/CartContext";
 function App() {
   const [preloader, setpreloader] = useState(true);
   setTimeout(() => {
     setpreloader(false);
   }, 1400);
 
-  var cxt= useContext(CartContext);
 
   
   
