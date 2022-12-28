1be38d2a52bc7eaa23a5409082c83effaa2207fa
Shashank Raj <shashank3636@gmail.com>
2022-12-29 00:21:05 +0530

    eighth

--- src/Components/Modal/ModalHolder.js
+++ src/Components/Modal/ModalHolder.js
@@ -1,8 +1,11 @@
 import React from "react";
+import CartItem from "./CartItem";
 import Modal from "./Modal";
+import "./ModuleHolder.css"
 
 const ModalHolder = (props) => {
     var modal = props.modalName;
+   
     const closeModal=()=>{
         props.onClose();
     }
@@ -10,52 +13,45 @@ const ModalHolder = (props) => {
     return (
         <React.Fragment>
             {modal === "projects" &&
-                <Modal onClose={closeModal} c
+                <Modal onClose={closeModal}
                 >
-                    <h2>Projects</h2>
+                    <h2 className="title_name" >Projects</h2>
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                   
+                        <CartItem id="1" title="Food Order App" describe="An app allows you to order food." />
+                        <CartItem id="2" title="Expense Handler" describe="Allows you to handle expenses." />
+                        <CartItem id="3" title="Typo Application" describe="Type writer application." />
+                        <CartItem id="4" title="Calculator App" describe="Themed Calculator" />
+                        <CartItem id="5" title="Netwiz" describe="ISP website." />
+                    </ul>  
                 </Modal>
             }
             {modal === "skills" &&
                 <Modal onClose={closeModal} >
-                    <h2>Skills</h2>
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
+                    <h2 className="title_name" >Skills</h2>
+                    
                 </Modal>
             }
             {modal === "experience" &&
                 <Modal onClose={closeModal} >
-                    <h2>Experience</h2>
+                    <h2 className="title_name" >Experience</h2>
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        
+                        
+                        
+                        
+                        
                     </ul>
                 </Modal>
             }
             {modal === "achievements" &&
                 <Modal onClose={closeModal} >
-                    <h2>Achievements</h2>
+                    <h2 className="title_name" >Achievements</h2>
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        
+                        
+                        
+                        
+                        
                     </ul>
                 </Modal>
             }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -18,7 +18,7 @@ import styles from "./NavBar.module.css"
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import AdbIcon from '@mui/icons-material/Adb';
-const icons = [<GitHubIcon href="https://github.com/sosenkkk" />, <LinkedInIcon href="https://www.linkedin.com/in/shashank-raj-5bb2a11b6/" />]
+const icons = [<a className={styles.link_navbar} href='https://github.com/sosenkkk' ><GitHubIcon /></a>,<a className={styles.link_navbar} href="https://www.linkedin.com/in/shashank-raj-5bb2a11b6/"><LinkedInIcon /></a>]
 
 const drawerWidth = 240;
 const navItems = ['Resume', 'About', 'Home'];
