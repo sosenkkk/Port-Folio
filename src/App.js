5b49ba335792f21cfc04fd1c20251c44276944ba
Shashank Raj <shashankraj3636@gmail.com>
2023-01-04 14:04:53 +0530

    fa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css"
-import React, { useState } from "react";
+import React, {  useState } from "react";
 import ResponsiveAppBar from "./Components/NavBar";
 import FrontPageAnimation from "./Components/StartUpAnimation/FrontPageAnimation";
 import CardHolder from "./Components/Card/CardHolder";
 import Preloader from "./Components/PreLoader/Preloader";
 import AboutMe from "./Components/About Me/AboutMe";
 import Footer from "./Components/Footer/Footer";
-
 function App() {
   const [preloader, setpreloader] = useState(true);
   setTimeout(() => {
@@ -14,18 +13,16 @@ function App() {
   }, 1400);
 
 
-
-
+  
+  
 
   return 
     {preloader && <Preloader />}
     {!preloader && <React.Fragment>
       <FrontPageAnimation />
       <ResponsiveAppBar />
-      <div className="encloser-div" >
-        <AboutMe />
-        <CardHolder />
-      </div>
+      <AboutMe />
+      <CardHolder />
       <Footer />
     </React.Fragment>}
   </CartProvider>
--- src/Components/Card/CardHolder.js
+++ src/Components/Card/CardHolder.js
@@ -13,7 +13,7 @@ const CardHolder = () => {
         setmodal('');
     }
     return (
-        <div style={{  display: "flex", justifyContent:"space-around", margin:"1rem 1rem 0 1rem", flexWrap:"wrap", backgroundColor:"black" }} >
+        <div style={{  display: "flex", justifyContent:"space-around", margin:"1rem", flexWrap:"wrap", backgroundColor:"black" }} >
             <ActionAreaCard onClick={showModal} id='projects' toDisplay="Projects" toDescribe="Few Projects I've done last year with everything I have learnt along the journey." todo="projects" source="https://i.pinimg.com/originals/97/ae/28/97ae288bc10481a0152460e9a3c5faeb.gif" />
             <ActionAreaCard onClick={showModal} id='skills' toDisplay="Skills" toDescribe="Languages and frameworks I have knowledge of as well as tools I use to implement them." todo="hobby" source="https://i.pinimg.com/originals/59/87/1c/59871c7fb4ca4d906e9ef1f4566cd378.gif" />
             <ActionAreaCard onClick={showModal} id='achievements' toDisplay="Scholastic Achievements" toDescribe="Here are a few things I've achieved." todo="achieve" source="https://i.pinimg.com/originals/a6/53/12/a653125902cbc01ad4f1198d33aadfc6.gif" />
