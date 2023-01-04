b2a73aedaf03ed823457dc071b3c06c6a53f42d5
Shashank Raj <shashankraj3636@gmail.com>
2023-01-04 14:10:08 +0530

    Merge branch 'main' of https://github.com/sosenkkk/Port-Folio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css"
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import ResponsiveAppBar from "./Components/NavBar";
 import FrontPageAnimation from "./Components/StartUpAnimation/FrontPageAnimation";
 import CardHolder from "./Components/Card/CardHolder";
 import Preloader from "./Components/PreLoader/Preloader";
 import AboutMe from "./Components/About Me/AboutMe";
+import CartProvider from "./Components/Store/CartProvider";
 import Footer from "./Components/Footer/Footer";
+
 function App() {
   const [preloader, setpreloader] = useState(true);
   setTimeout(() => {
@@ -13,20 +15,21 @@ function App() {
   }, 1400);
 
 
-  
-  
 
-  return 
+
+
+  return <CartProvider>
     {preloader && <Preloader />}
     {!preloader && <React.Fragment>
       <FrontPageAnimation />
       <ResponsiveAppBar />
-      <AboutMe />
-      <CardHolder />
+      <div className="encloser-div" >
+        <AboutMe />
+        <CardHolder />
+      </div>
       <Footer />
     </React.Fragment>}
   </CartProvider>
 }
 
 export default App;
-
