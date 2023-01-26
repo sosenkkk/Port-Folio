bf9f294dad593a765e825b5b43d16637fc278c95
Shashank Raj <shashankraj3636@gmail.com>
2023-01-26 09:30:47 +0530

    port

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -17,11 +17,11 @@ const Footer = () => {
                         <h2>Contact Me</h2>
                     </div>
                     <div className="footer_mine" >
-                        <motion.a drag dragConstraints={{left:10, right:10, top:0, bottom:0}} whileHover={{rotate:[0, 30, -50, 0]}} transition={{ type:spring, duration:0.5, bounce:5}}  href="mailto:shashankraj3636@gmail.com" rel="noopener noreferrer" target="_blank" ><GoogleIcon fontSize="large" /></motion.a>
-                        <motion.a drag dragConstraints={{left:10, right:10, top:0, bottom:0}} whileHover={{rotate:[0, 30, -50, 0]}} transition={{ type:spring, duration:0.5, bounce:5}} target="_blank" rel="noopener noreferrer" href='https://github.com/sosenkkk' ><GitHubIcon fontSize="large" /></motion.a>
-                        <motion.a drag dragConstraints={{left:10, right:10, top:0, bottom:0}} whileHover={{rotate:[0, 30,-50, 0]}} transition={{ type:spring, duration:0.5, bounce:5}} target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/shashank-raj-5bb2a11b6/"><LinkedInIcon fontSize="large" /></motion.a>
-                        <motion.a drag dragConstraints={{left:10, right:10, top:0, bottom:0}} whileHover={{rotate:[0, 30, -50, 0]}} transition={{ type:spring, duration:0.5, bounce:5}} target="_blank" rel="noopener noreferrer" href='https://twitter.com/sosenkkk' ><TwitterIcon fontSize="large" /></motion.a>
-                        <motion.a drag dragConstraints={{left:10, right:10, top:0, bottom:0}} whileHover={{rotate:[0, 30, -50, 0]}} transition={{ type:spring, duration:0.5, bounce:5}} target="_blank" rel="noopener noreferrer" href='https://www.instagram.com/sosenkkk/' ><InstagramIcon fontSize="large" /></motion.a>
+                        <motion.a drag dragConstraints={{left:10, right:10, top:0, bottom:0}} whileHover={{rotate:[0, 360]}} transition={{ type:spring, duration:0.3, bounce:5}}  href="mailto:shashankraj3636@gmail.com" rel="noopener noreferrer" target="_blank" ><GoogleIcon fontSize="large" /></motion.a>
+                        <motion.a drag dragConstraints={{left:10, right:10, top:0, bottom:0}} whileHover={{rotate:[0, 360]}} transition={{ type:spring, duration:0.3, bounce:5}} target="_blank" rel="noopener noreferrer" href='https://github.com/sosenkkk' ><GitHubIcon fontSize="large" /></motion.a>
+                        <motion.a drag dragConstraints={{left:10, right:10, top:0, bottom:0}} whileHover={{rotate:[0, 360]}} transition={{ type:spring, duration:0.3, bounce:5}} target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/shashank-raj-5bb2a11b6/"><LinkedInIcon fontSize="large" /></motion.a>
+                        <motion.a drag dragConstraints={{left:10, right:10, top:0, bottom:0}} whileHover={{rotate:[0, 360]}} transition={{ type:spring, duration:0.3, bounce:5}} target="_blank" rel="noopener noreferrer" href='https://twitter.com/sosenkkk' ><TwitterIcon fontSize="large" /></motion.a>
+                        <motion.a drag dragConstraints={{left:10, right:10, top:0, bottom:0}} whileHover={{rotate:[0, 360]}} transition={{ type:spring, duration:0.3, bounce:5}} target="_blank" rel="noopener noreferrer" href='https://www.instagram.com/sosenkkk/' ><InstagramIcon fontSize="large" /></motion.a>
                     </div>
                 </div>
             </motion.div>
